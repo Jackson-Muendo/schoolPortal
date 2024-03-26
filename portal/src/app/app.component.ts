b98237d55ca45d03b631684a9b9217252d32eec9
Jackson-Muendo <jackmwexh1225@gmail.com>
2024-03-26 22:49:08 +0300

    portal

--- portal/src/app/app.component.ts
+++ portal/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AddstudentsComponent } from './addstudents/addstudents.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,HeaderComponent,FooterComponent,
-    AddstudentsComponent,DashboardComponent
+    AddstudentsComponent,DashboardComponent,MenuComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
